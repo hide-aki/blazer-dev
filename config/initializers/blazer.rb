15cd41b54b47a0dca415c96f729cb6896ff8dab5
Andrew Kane <acekane1@gmail.com>
2016-10-17 20:07:43 -0700

    Prevent access to system tables in demo

--- config/initializers/blazer.rb
+++ config/initializers/blazer.rb
@@ -6,24 +6,34 @@ module DemoModel
   end
 
   def prevent_save
-    errors.add(:base, "Sorry, not in the demo") if changed? && ENV["DEMO"].present?
+    errors.add(:base, "Sorry, not in the demo") if changed?
   end
 
   def destroy
-    super unless ENV["DEMO"].present?
   end
 end
 
-[
-  Blazer::Query,
-  Blazer::Dashboard,
-  Blazer::DashboardQuery,
-  Blazer::Check
-].each do |model|
-  model.send(:include, DemoModel)
+module DemoRunStatement
+  def perform(data_source, statement, options = {})
+    if statement.include?("pg_") || statement.include?("information_schema")
+      statement = "SELECT 1 LIMIT 0" # empty results
+    end
+    super(data_source, statement, options)
+  end
 end
 
 if ENV["DEMO"].present?
+  [
+    Blazer::Query,
+    Blazer::Dashboard,
+    Blazer::DashboardQuery,
+    Blazer::Check
+  ].each do |model|
+    model.send(:include, DemoModel)
+  end
+
+  Blazer::RunStatement.prepend(DemoRunStatement)
+
   class Blazer::Check
     def update_state(*args)
       # do nothing
